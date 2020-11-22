051e6137508a382d2f4e6da7678cf6011aef55b4
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-11-22 20:14:04 +0300

    Database Created

--- Hff.Challenge.DataAccess/Concrete/Context/ChallengeContext.cs
+++ Hff.Challenge.DataAccess/Concrete/Context/ChallengeContext.cs
@@ -1,4 +1,5 @@
-﻿using Hff.Challenge.Entities.Concrete;
+﻿using Hff.Challenge.DataAccess.Concrete.Mappings;
+using Hff.Challenge.Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,8 +9,19 @@ namespace Hff.Challenge.DataAccess.Concrete.Context
 {
     public class ChallengeContext:DbContext
     {
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=localhost\\SQLEXPRESS;Database=Challenge;Integrated Security=true");
+            base.OnConfiguring(optionsBuilder);
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfiguration(new AppUserMap());
+            modelBuilder.ApplyConfiguration(new ProductMap());
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+
+
             base.OnModelCreating(modelBuilder);
         }
         public DbSet<AppUser> AppUsers { get; set; }
